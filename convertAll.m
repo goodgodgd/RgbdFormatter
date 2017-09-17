aec82887d9ddec56e9e91d497086ee9bd6346584
goodgodgd <goodgodgd@yonsei.ac.kr>
2017-09-17 14:43:39 +0900

    ...

--- convertAll.m
+++ convertAll.m
@@ -2,18 +2,18 @@ clc
 clear
 
 %%%% ScanNet
-% sourcePath = '/media/cideep/HardDisk/RGBD-datasets-raw/scannet/scene-frames';
-% sceneDirPattern = 'sceneimgs_*';
-% targetPath = '/media/cideep/HardDisk/RGBD-datasets-formatted/scannet';
-% rgbdFormatter('scannet', sourcePath, targetPath, sceneDirPattern)
+sourcePath = '/media/cideep/HardDisk/RGBD-datasets-raw/scannet/scene-frames';
+sceneDirPattern = 'sceneimgs_*';
+targetPath = '/media/cideep/HardDisk/RGBD-datasets-formatted/scannet';
+rgbdFormatter('scannet', sourcePath, targetPath, sceneDirPattern)
 
-% %%%%% TUM
-% sourcePath = '/media/cideep/HardDisk/RGBD-datasets-raw/TUM-SLAM';
-% sceneDirPattern = 'rgbd_dataset_freiburg*';
-% targetPath = '/media/cideep/HardDisk/RGBD-datasets-formatted/tum';
-% rgbdFormatter('tum', sourcePath, targetPath, sceneDirPattern)
+%%%%% TUM
+sourcePath = '/media/cideep/HardDisk/RGBD-datasets-raw/TUM-SLAM';
+sceneDirPattern = 'rgbd_dataset_freiburg*';
+targetPath = '/media/cideep/HardDisk/RGBD-datasets-formatted/tum';
+rgbdFormatter('tum', sourcePath, targetPath, sceneDirPattern)
 
-% %%%%% rgbd-scenes
+%%%%% rgbd-scenes
 sourcePath = '/media/cideep/HardDisk/RGBD-datasets-raw/rgbd-scenes-v2/imgs';
 sceneDirPattern = 'scene_*';
 targetPath = '/media/cideep/HardDisk/RGBD-datasets-formatted/rgbd-scenes-v2';
